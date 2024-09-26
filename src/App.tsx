55b864b8afc2df69269bd422da9374059cf517fa
samarth <samarthdevlibtech@gmail.com>
2024-09-26 16:11:21 +0530

    HEADER_FOOTER_UPDATED

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainTopHeader from './common/top-header';
+import { MainFooter } from './common/main-footer';
 
 function App() {
   return (
     <Router>
+      <MainTopHeader/>
       <Routes>
-        <Route path="/" element={<>Istitch Digitizing Website</>} />
+        {/* <Route path="/" element={<>Istitch Digitizing Website</>} /> */}
       </Routes>
+      <MainFooter/>
     </Router>
   );
 }
